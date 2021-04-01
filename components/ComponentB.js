f9051c942b7a318b54ae9ab74bfb92ed08adbc02
Dongyu Kang <dongyukang@naver.com>
2021-04-01 14:27:44 +0000

    Navigation Container

---
+++ components/ComponentB.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ComponentB = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default ComponentB;
