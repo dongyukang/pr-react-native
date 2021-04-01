24d161bb942d67ba6efcf9f869ddd456d899ce68
Dongyu Kang <dongyukang@naver.com>
2021-04-01 14:38:52 +0000

    a lot of changes

--- App.js
+++ App.js
@@ -5,12 +5,18 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Home from './components/Home.js';
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
           <Stack.Navigator>
+              <Stack.Screen
+                name="Home"
+                component={ Home } 
+              />
           </Stack.Navigator>
       </NavigationContainer>
   );
---
+++ components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                A text is here
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
